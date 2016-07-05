ef08f70283c150b466974b4979cd37a83b472e39
seguelador <matias.seguel.estay@gmail.com>
2016-07-04 20:31:09 -0400

    Se agrega formulario que agrega usuario a la base de datos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def inicio
   end
+
+  def save_user
+  	@name = params[:name]
+  	@email = params[:email]
+  	@age = params[:age]
+  	@user = User.new(name: @name, email: @email, age: @age)
+  	if @user.save
+  		@response = "Se ha guardado los datos correctamente"
+  	else
+  		@response = "Ocurrió un error al intentar guardar"  		
+  	end
+  end
 end
